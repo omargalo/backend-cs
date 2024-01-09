bddab18b309e92720db374ee9647c6538ea41e0c
Omar García López <omar.garcia@omargl.net>
2024-01-09 15:16:39 -0600

    Inyección del contexto

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,22 +1,31 @@
+using Backend.Models;
 using Backend.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddSingleton<IPeopleService, people2Service>();
+// builder.Services.AddSingleton<IPeopleService, people2Service>();
 builder.Services.AddKeyedSingleton<IPeopleService, peopleService>("peopleService");
 builder.Services.AddKeyedSingleton<IPeopleService, people2Service>("people2Service");
 
-//Different Dependency Injection Types
+// Different Dependency Injection Types
 builder.Services.AddKeyedSingleton<IRandomService, RandomService>("randomSingleton");
 builder.Services.AddKeyedScoped<IRandomService, RandomService>("randomScoped");
 builder.Services.AddKeyedTransient<IRandomService, RandomService>("randomTransient");
 
 builder.Services.AddScoped<IPostsService, PostsService>();
 
+// HttpClient service JsonPlaceholder
 builder.Services.AddHttpClient<IPostsService, PostsService>(c =>
 {
     c.BaseAddress = new Uri(builder.Configuration["BaseUrlPosts"]);
+});
+
+// Entity Framework
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StoreConnection"));
 });
 
 builder.Services.AddControllers();
