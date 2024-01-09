fad699b13253db6d97525bfd2f9f05ffb37e6d40
Omar García López <omar.garcia@omargl.net>
2024-01-09 12:23:12 -0600

    Métodos asíncronos

--- Backend/Controllers/SomethingController.cs
+++ Backend/Controllers/SomethingController.cs
@@ -30,6 +30,9 @@ namespace Backend.Controllers
         [HttpGet("async")]
         public async Task<IActionResult> GetAsync()
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
             var task1 = new Task<int>(() =>
             {
                 Thread.Sleep(1000);
@@ -37,14 +40,26 @@ namespace Backend.Controllers
                 return 7;
            });
 
+            var task2 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Correo 2 enviado");
+                return 2;
+            });
+
             task1.Start();
+            task2.Start();
 
             Console.WriteLine("Hago otra cosa");
 
             var result1 = await task1;
+            var result2 = await task2;
+
+
             Console.WriteLine("Todo ha terminado");
 
-            return Ok(result1);
+            stopwatch.Stop();
+            return Ok(result1 + " " + result2 + " " + stopwatch.Elapsed);
         }
     }
 }
