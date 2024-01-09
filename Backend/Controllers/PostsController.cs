6e89fbddb97ae97877b297f6b1a68af6f35cf27b
Omar García López <omar.garcia@omargl.net>
2024-01-09 14:22:25 -0600

    Http Client solicitudes externas

---
+++ Backend/Controllers/PostsController.cs
@@ -0,0 +1,23 @@
+﻿using Backend.DTOs;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        IPostsService _titlesService;
+
+        public PostsController(IPostsService titlesService)
+        {
+            _titlesService = titlesService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PostDto>> Get() =>
+            await _titlesService.Get();
+    }
+}
---
+++ Backend/Services/PostsService.cs
@@ -0,0 +1,32 @@
+﻿using Backend.DTOs;
+using System.Text.Json;
+
+namespace Backend.Services
+{
+    public class PostsService : IPostsService
+    {
+        private HttpClient _httpClient;
+
+        //Constructor
+        public PostsService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IEnumerable<PostDto>> Get()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            var result = await _httpClient.GetAsync(url);
+            var body = await result.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var post = JsonSerializer.Deserialize<IEnumerable<PostDto>>(body, options);
+
+            return post;
+        }
+    }
+}
