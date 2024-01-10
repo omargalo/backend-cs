46f68f5853aef9d6083871c3774f683598148834
Omar García López <omar.garcia@omargl.net>
2024-01-10 11:02:53 -0600

    Custom error messages

--- Backend/Validators/BeerInsertValidator.cs
+++ Backend/Validators/BeerInsertValidator.cs
@@ -7,7 +7,11 @@ namespace Backend.Validators
     {
         public BeerInsertValidator()
         {
-            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().WithMessage("El nombre es obligatorio");
+            RuleFor(x => x.Name).Length(2,20).WithMessage("El nombre debe tener entre 2 y 20 caracteres");
+            RuleFor(x => x.BrandId).NotEmpty().WithMessage("La marca es obligatoria");
+            RuleFor(x => x.BrandId).GreaterThan(0).WithMessage(x => "Error con el valor de la marca");
+            RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage(x => "El {PropertyName} debe ser mayor a 0");
         }
     }
 }
