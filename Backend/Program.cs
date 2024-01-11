8a07468f552b3aee90bdec7c774e4481c23883a2
Omar García López <omar.garcia@omargl.net>
2024-01-10 20:41:33 -0600

    Install AutoMapper.Extensions.Microsoft.DependencyInjection

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,4 @@
+using Backend.Automappers;
 using Backend.DTOs;
 using Backend.Models;
 using Backend.Repository;
@@ -38,6 +39,9 @@ builder.Services.AddDbContext<StoreContext>(options =>
 // Validators
 builder.Services.AddScoped<IValidator<BeerInsertDto> , BeerInsertValidator>();
 builder.Services.AddScoped<IValidator<BeerUpdateDto>, BeerUpdateValidator>();
+
+// Mapping
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
